25af416fc386d87ebd734c73c17ec8df448ba242
obenedek20 <obenedek20@users.noreply.github.com>
2024-10-08 19:41:05 -0700

    everythibng but hashcode is done

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,10 +10,11 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
+    Team team2;
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team"); 
     }
 
     @Test
@@ -19,8 +22,48 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_same_obj() {
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_not_same_obj() {
+        Object o = new Object();
+        assertFalse(team.equals(o));
+    }
+
+    @Test
+    public void equals_same_name_diff_members() {
+        team2 = new Team("test-team");
+        team2.addMember("diff_member");
+        assertFalse(team.equals(team2));
+    }
+
+    @Test
+    public void equals_same_name_same_members() {
+        team2 = new Team("test-team");
+        team2.setMembers(team.getMembers());
+        assertTrue(team.equals(team2));
+    }
+
+    @Test
+    public void equals_diff_name_same_members() {
+        team2 = new Team("test-name-2");
+        team2.setMembers(team.getMembers());
+        assertFalse(team.equals(team2));
+    }
+
+    @Test
+    public void equals_diff_name_diff_members() {
+        team2 = new Team("test-name-2");
+        team2.addMember("diff_member");
+        assertFalse(team.equals(team2));
+    }
 
 }
